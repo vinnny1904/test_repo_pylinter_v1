aa2f04d69abb17434a59891848f0c871157e555a
vinnny1904 <116699064+vinnny1904@users.noreply.github.com>
2026-05-20 16:42:00 +0800

    add new function

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 def add(a, b):
     return a + b
 
+def multiply(a, b):
+    return a * b
+
 
 if __name__ == "__main__":
-    result = add(2, 3)
-    print(f"Result: {result}")
+    add_result = add(2, 3)
+    multi_result = multiply(2, 2)
+    print(f"Result: add = {add_result} - multiply = {multi_result}")
